a90f04588dcce1d8e6545173392875d6ad5ac22c
EMU4729 <32506926+unswfrc@users.noreply.github.com>
2026-04-11 00:55:50 +1000

    use auto rotation pid constants in auto aim command

--- src/main/java/frc/robot/commands/autoaim/AutoAim.java
+++ src/main/java/frc/robot/commands/autoaim/AutoAim.java
@@ -1,23 +1,18 @@
 package frc.robot.commands.autoaim;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Subsystems;
 import frc.robot.constants.AimingConstants;
-import frc.robot.constants.AutoAlignConstants;
-import frc.robot.subsystems.DriveSub;
 import frc.robot.utils.TurretAiming;
 
 public class AutoAim extends Command {
-  // todo tune these
-  private final PIDController turnController = new PIDController(3, 0, 0);
+  // PID constants same as DriveConstants.AUTO_ROTATION_PID
+  private final PIDController turnController = new PIDController(5, 0.5, 0.1);
   private Translation2d hubPosition;
 
   public AutoAim() {
@@ -41,7 +36,7 @@ public class AutoAim extends Command {
   @Override
   public void execute() {
     final var targetTransform = TurretAiming.getTargetTransform(hubPosition);
-   // System.out.println(targetTransform.toString());
+    // System.out.println(targetTransform.toString());
     final var error = targetTransform.getRotation().getRadians();
     final var out = turnController.calculate(error);
     // System.out.println(out);
